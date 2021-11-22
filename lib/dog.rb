c3591452dd51029d199642e61ccfe2700f70a40d
Yuyi <yuyi365@gmail.com>
2021-11-22 11:55:39 -0500

    commit tree 7f39f774537ff996cb2b43f0c81e5bb191e1b68f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    #getter
+    def name
+        @name
+    end
+
+    #setter
+    def name=(name)
+        @name = name
+    end
+
+    #getter
+    def breed
+        @breed
+    end
+    
+    #setter
+    def breed=(breed)
+        @breed = breed
+    end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    #setter
+    def name = (name)
+        @name = name
+    end
+
+    #getter
+    def name
+        @name
+    end
+
+    #setter
+    def job = (job)
+        @job = job
+    end
+
+    #getter
+    def job
+        @job
+    end
+
 end
